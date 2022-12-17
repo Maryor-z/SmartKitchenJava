21a8e69ddaed61b14af7697bfc97d4ed880e1138
Maryor-z <88011448+Maryor-z@users.noreply.github.com>
2022-12-17 09:35:11 +0100

    Update SmarkKitchen.java

--- src/SmarkKitchen.java
+++ src/SmarkKitchen.java
@@ -1,2 +1,87 @@
-package PACKAGE_NAME;public class SmarkKitchen {
+import java.awt.*;
+
+public class SmarkKitchen {
+
+    private CoffeeMaker brewMaster;
+    private Refrigerator iceBox;
+    private Dishwasher dishwasher;
+
+    public SmarkKitchen(){
+        brewMaster = new CoffeeMaker();
+        iceBox = new Refrigerator();
+        dishwasher = new Dishwasher();
+    }
+
+    public CoffeeMaker getBrewMaster() {
+        return brewMaster;
+    }
+
+    public Refrigerator getIceBox() {
+        return iceBox;
+    }
+
+    public Dishwasher getDishwasher() {
+        return dishwasher;
+    }
+
+    public void setKitchenState(boolean coffeeFlag, boolean fridgeFlag,
+                                boolean dishWasherFlag) {
+        brewMaster.setHasWorkToDo(coffeeFlag);
+        iceBox.setHasWorkToDo(fridgeFlag);
+        dishwasher.setHasWorkToDo(dishWasherFlag);
+    }
+
+    public void doKitchenWork(){
+        brewMaster.brewCoffee();
+        iceBox.orderFood();
+        dishwasher.doDishes();
+    }
+}
+
+class CoffeeMaker{
+
+    private boolean hasWorkToDo;
+
+    public void setHasWorkToDo(boolean hasWorkToDo) {
+        this.hasWorkToDo = hasWorkToDo;
+    }
+
+    public void brewCoffee(){
+        if (hasWorkToDo){
+            System.out.println("Brewing Coffee");
+            hasWorkToDo = false;
+        }
+    }
+}
+
+class Refrigerator{
+
+    private boolean hasWorkToDo;
+
+    public void setHasWorkToDo(boolean hasWorkToDo) {
+        this.hasWorkToDo = hasWorkToDo;
+    }
+
+    public void orderFood(){
+        if (hasWorkToDo){
+            System.out.println("Ordering food");
+            hasWorkToDo = false;
+        }
+    }
+}
+
+class Dishwasher {
+
+    private boolean hasWorkToDo;
+
+    public void setHasWorkToDo(boolean hasWorkToDo) {
+        this.hasWorkToDo = hasWorkToDo;
+    }
+
+    public void doDishes(){
+        if (hasWorkToDo){
+            System.out.println("washing dishes");
+            hasWorkToDo = false;
+        }
+    }
 }
