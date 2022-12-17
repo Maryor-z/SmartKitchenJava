f8c6ef8dad3dbf80c397bf0d587c032426db3552
Maryor-z <88011448+Maryor-z@users.noreply.github.com>
2022-12-17 09:35:56 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        SmarkKitchen kitchen = new SmarkKitchen();
+
+//        kitchen.getDishwasher().setHasWorkToDo(true);
+//        kitchen.getIceBox().setHasWorkToDo(true);
+//        kitchen.getBrewMaster().setHasWorkToDo(true);
+//        kitchen.getDishwasher().doDishes();
+//        kitchen.getIceBox().orderFood();
+//        kitchen.getBrewMaster().brewCoffee();
+
+        kitchen.setKitchenState(true, true, false);
+        kitchen.doKitchenWork();
+
+    }
 }
